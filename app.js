6f987cf209d7d2f5a59cab115d5637f12d7393c9
sakshi khullar <khullarsakshi5@gmail.com>
2024-07-01 00:40:25 +0530

    Files are added

---
+++ app.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+btn.addEventListener("click", function () {
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    ul.appendChild(item);
+    inp.value = "";
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+    item.appendChild(delbtn)
+});
+
+ul.addEventListener("click", function (event) {
+    if (event.target.nodeName == "BUTTON") {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    }
+});
